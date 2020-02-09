af3044c0aeb6d59032cd5a2a6b77fe6015f16b9f
AnnAngela <AnnAngela@users.noreply.github.com>
2020-02-09 17:01:02 +0800

    Update MoeUpload.i18n.php

--- MoeUpload/MoeUpload.i18n.php
+++ MoeUpload/MoeUpload.i18n.php
@@ -14,7 +14,7 @@ $messages['zh-hant'] = array(
 	'moemoeQCharName' => '人物名',
 	'moemoeQAuthor' => '作者',
 	'moemoeQSrcUrl' => '源地址',
-	'moemoeQdec' => '[http://zh.moegirl.org/ 萌娘百科]式上傳頁面註釋增強插件'
+	'moemoeQdec' => '[http://zh.moegirl.org/ 萌娘百科]式上傳頁面增強插件'
 );
 
 /* English */
