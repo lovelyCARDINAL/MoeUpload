5aba62ba72cfa132010e72bac50f1a192f5f43f5
星海 <69918945+lovelyCARDINAL@users.noreply.github.com>
2022-12-09 12:56:47 +0800

    fix: moemoeQdec

--- MoeUpload/MoeUpload.i18n.php
+++ MoeUpload/MoeUpload.i18n.php
@@ -7,21 +7,21 @@ $messages['zh-hans'] = array(
 	'moemoeQCharName' => '人物名',
 	'moemoeQAuthor' => '作者',
 	'moemoeQSrcUrl' => '源地址',
-	'moemoeQdec' => '[http://zh.moegirl.org/ 萌娘百科]上传文件页面增强插件'
+	'moemoeQdec' => '[https://zh.moegirl.org.cn/ 萌娘百科]上传文件页面增强插件'
 );
 
 $messages['zh-hant'] = array(
 	'moemoeQCharName' => '人物名',
 	'moemoeQAuthor' => '作者',
 	'moemoeQSrcUrl' => '源地址',
-	'moemoeQdec' => '[http://zh.moegirl.org/ 萌娘百科]上傳檔案頁面增強插件'
+	'moemoeQdec' => '[https://zh.moegirl.org.cn/ 萌娘百科]上傳檔案頁面增強插件'
 );
 
 $messages['zh-hk'] = array(
 	'moemoeQCharName' => '人物名',
 	'moemoeQAuthor' => '作者',
 	'moemoeQSrcUrl' => '源地址',
-	'moemoeQdec' => '[http://zh.moegirl.org/ 萌娘百科]上載檔案頁面增強插件'
+	'moemoeQdec' => '[https://zh.moegirl.org.cn/ 萌娘百科]上載檔案頁面增強插件'
 );
 
 /* English */
@@ -29,7 +29,7 @@ $messages['en'] = array(
 	'moemoeQCharName' => 'Character name',
 	'moemoeQAuthor' => 'Author',
 	'moemoeQSrcUrl' => 'Origin Source URL',
-	'moemoeQdec' => '[http://en.moegirl.org/ MoegirlPedia] style uploadpage enhance.'
+	'moemoeQdec' => '[https://en.moegirl.org.cn/ MoegirlPedia] style uploadpage enhance.'
 );
 
 /* Japanese */
@@ -38,6 +38,6 @@ $messages['ja'] = array(
 	'moemoeQCharName' => '名前',
 	'moemoeQAuthor' => '絵師',
 	'moemoeQSrcUrl' => 'ソースアドレス',
-	'moemoeQdec' => '[http://ja.moegirl.org/ 萌えっ娘百科]形式アップロード強化する。'
+	'moemoeQdec' => '[https://ja.moegirl.org.cn/ 萌えっ娘百科]形式アップロード強化する。'
 );
 ?>
